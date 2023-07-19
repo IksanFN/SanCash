4dacc9994c50afc03b4131a0aec3b5bf064d366b
IksanFN <iksanfauzi727@gmail.com>
2023-07-19 21:08:13 +0700

    Export Excel Transaction

---
+++ app/Exports/TransactionsExport.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Transaction;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromCollection;
+// use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class TransactionsExport implements FromQuery, WithMapping, WithHeadings, ShouldAutoSize, WithStyles
+{
+    use Exportable;
+
+    public $idBill;
+
+    public function headings(): array
+    {
+        return [
+            'NISN',
+            'Nama',
+            'Jurusan',
+            'Kelas',
+            'Tagihan',
+            'Status',
+            'Tanggal Bayar',
+        ];
+    }
+
+    public function __construct($idBill)
+    {
+        $this->idBill = $idBill;
+    }
+
+    public function query()
+    {
+        return Transaction::query()->whereBillId($this->idBill);
+    }
+
+    public function map($transactions): array
+    {
+        return [
+            $transactions->student->user->nisn,
+            $transactions->student->user->name,
+            $transactions->student->jurusan->name,
+            $transactions->student->kelas->name,
+            $transactions->bill->bill,
+            $transactions->payment_status,
+            $transactions->payment_date
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]]
+        ];
+    }
+}
